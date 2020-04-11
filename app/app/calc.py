131ed8431e757f08ee88569fa97d8af6dfd40e94
Hahnsang Kim <hkim@inspro9.com>
2020-04-11 15:06:30 -0400

    Create User model and test

--- app/app/calc.py
+++
@@ -1,9 +0,0 @@
-def add(x, y):
-    """
-    a simple function for the purpose of unit-test exercise
-    """
-    return x + y
-
-def subtract(x, y):
-    """Subtract values and return"""
-    return y - x
